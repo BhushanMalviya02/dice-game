7c9532158c5947a5666008c7a36929385059fb5f
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-04-10 20:35:45 +0530

    Fix image path for dice images

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 var images1="./images/dice"+randomNumber1+".png";
-var images2="/images/dice"+randomNumber2+".png";
+var images2="./images/dice"+randomNumber2+".png";
 
 document.querySelector(".img1").setAttribute("src",images1);
 document.querySelector(".img2").setAttribute("src",images2);
